c8696783d283e30d8f1385f010029f50ded52ffe
Lenny Hicks <lenny.hicks@interapthq.com>
2018-04-06 16:59:33 -0400

    Broke the GetAllMemories

--- presentation/src/main/java/iskills/com/memories/ui/allMemories/ViewGetAllMemories.java
+++ presentation/src/main/java/iskills/com/memories/ui/allMemories/ViewGetAllMemories.java
@@ -1,9 +1,21 @@
 package iskills.com.memories.ui.allMemories;
 
-import java.util.List;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import iskills.com.domain.model.Memory;
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import iskills.com.memories.R;
+import iskills.com.memories.di.qualifiers.QualAdapterMemoryGrid;
 import iskills.com.memories.mvp.ViewBase;
+import iskills.com.memories.ui.adapters.AdapterMemoryGrid;
 
 /**
  * lennyhicks
@@ -11,9 +23,31 @@ import iskills.com.memories.mvp.ViewBase;
  */
 public class ViewGetAllMemories extends ViewBase implements ContractGetAllMemories.View {
 
-    @Override
-    public void showMemories(List<Memory> memories) {
+    @Inject ContractGetAllMemories.Presenter presenterGetAllMemories;
+    @Inject @QualAdapterMemoryGrid AdapterMemoryGrid adapterMemoryGrid;
 
+    @BindView(R.id.recycler_view)
+    public RecyclerView recyclerView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_memory_grid, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        recyclerView.setAdapter(adapterMemoryGrid);
+        presenterGetAllMemories.getAllMemories();
+    }
+
+    @Override
+    public void updateList() {
+        adapterMemoryGrid.notifyDataSetChanged();
+    }
 }
